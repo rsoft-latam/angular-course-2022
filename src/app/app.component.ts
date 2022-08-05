233994c1c2675939d3e863221558fbf455ca3711
rsoft-latam <rikiricardo16@hotmail.com>
2022-08-05 18:07:02 -0400

    [08-05-2022 task 2] examples of MAP, FOR EACH, FIND

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,18 @@ export class AppComponent {
 
   constructor() {
     console.log('hi');
+
+    const auxMap = [1,2,3,4,5].map(num => num * 2)
+    console.log("auxMap:", auxMap);
+
+    const auxForEach = [1,2,3,4,5].forEach(num => num * 2)
+    console.log("auxForEach:", auxForEach)
+
+    const auxFind = [1,2,3,4,5].find(num => num === 3)
+    console.log("auxFind:", auxFind)
+
+
+
   }
 
   private getName(name: string): string {
