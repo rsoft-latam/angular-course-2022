228b7c9b1bd843f8fa665e168b383eb50483b9fc
rsoft-latam <rikiricardo16@hotmail.com>
2022-08-05 17:44:39 -0400

    [05-08-2022 task 1] examples of arrow functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'test';
+  public title = 'test';
+
+  constructor() {
+    console.log('hi');
+  }
+
+  private getName(name: string): string {
+    return 'hi' + name;
+  }
+
+  // --- Arrow function
+  // const getName = (name: string) => {return "hi" + name;}
+  // const getName = (name: string) => "hi" + name
+  // const getName = () => "hi"
+  // const getName = (name:string, lasName:string) => lasName
+
+  // const mult = num => num*2
+  private mult(num: any) {
+    return num;
+  }
 }
