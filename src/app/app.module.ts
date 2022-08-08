2683ae91e2b07103c0e413c2b64ecd19cabcb38c
rsoft-latam <rikiricardo16@hotmail.com>
2022-08-08 18:44:29 -0400

    [08-08-2022 task 4] my first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstComponent } from './my-first/my-first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-first/my-first.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first',
+  templateUrl: './my-first.component.html',
+  styleUrls: ['./my-first.component.css']
+})
+export class MyFirstComponent {
+
+  constructor() { }
+
+}
