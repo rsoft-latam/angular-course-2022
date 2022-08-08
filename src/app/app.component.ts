ea11e404b5b1daaf737399492d31e0057a1ea6db
rsoft-latam <rikiricardo16@hotmail.com>
2022-08-08 17:28:43 -0400

    [08-08-2022 task 1] operator splice, reduce, json operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+
+  constructor() {
+    // Operator Splice
+    const A = ['a','b','c','d','e'];
+    const auxSplice = A.splice(1,1)
+    console.log('auxSplice:', A, auxSplice)
+
+    // Operador Reduce
+    const auxReduce = [1,2,3,4,5].reduce(
+      (prev, value, index)=> prev + value, 0
+    )
+    console.log("auxReduce: ", auxReduce)
+    
+    // JSON Operators 
+    const auxJSON = {
+      id: 1,
+      name: 'JUAN',
+      lasName: 'GUTIERREZ'
+    };
+
+    // ENTRIES, KEY, VALUES
+
+    const auxentries = Object.entries(auxJSON)
+    console.log("auxentries:", auxentries)
+
+    const auxKeys = Object.keys(auxJSON)
+    console.log("auxKeys:", auxKeys)
+
+    const auxValues = Object.values(auxJSON)
+    console.log("auxValues:", auxValues)   
+  }
+
 }
