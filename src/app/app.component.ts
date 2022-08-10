13185612e3bf7fa8fb7d46cf0bd845bb8556cef0
rsoft-latam <rikiricardo16@hotmail.com>
2022-08-10 18:22:21 -0400

    [10-08-2022 task 3] two way binding examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,11 @@ export class AppComponent {
   title = 'test';
   public person!: IPerson;
 
+  public student: string = 'example juan.'
+  
+  public a = 0;
+  public b = 0;
+
   getPrint(event:IPerson, idPerson?:string) {
     console.log('HI PARENT COMPONENT:', event)
     this.person = event;
