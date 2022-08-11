25d59bc7dc95f437e86c3f52e9134d4f4debfea6
rsoft-latam <rikiricardo16@hotmail.com>
2022-08-11 18:05:57 -0400

    [11-08-2022 task 2] On Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test';
+  public name:string = "maria";
+  public lastName:string = "gutierrez";
 }
--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,23 +1,34 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-test1',
   templateUrl: './test1.component.html',
-  styleUrls: ['./test1.component.css']
+  styleUrls: ['./test1.component.css'],
 })
 export class Test1Component implements OnInit, OnChanges {
+  @Input() name!: string;
+  @Input() lastName: string = "perez";
 
-  constructor() { 
-    console.log('constructor')
+  constructor() {
+    console.log('constructor');
   }
 
-  ngOnChanges():void{
-    console.log('on changes')
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('on changes', changes, changes['name'].isFirstChange());
+
+    if(changes['name'].currentValue !== undefined && !changes['name'].isFirstChange()){
+      console.log('call country service')
+    }
+
   }
 
-  ngOnInit():void {
-    console.log('on init')
+  ngOnInit(): void {
+    console.log('on init');
   }
-
-  
 }
