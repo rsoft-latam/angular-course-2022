922700f8bce8f30429178cab4e0882dbe9ad5674
rsoft-latam <rikiricardo16@hotmail.com>
2022-08-18 19:13:58 -0400

    [18-08-2022 task 4] default pipes in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,12 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class AppComponent {
   title = 'test';
 
+  data = {
+    id: 1,
+    name: 'maria',
+    lastName: 'perez'
+  }
+
   //@ViewChild('myText', {static: false}) myTest!: ElementRef;
   @ViewChild('myText') myTest!: ElementRef;
 
