dd5274526fb5cc1c5d0d7ce8514d4938f44af75f
rsoft-latam <rikiricardo16@hotmail.com>
2022-08-18 17:15:12 -0400

    [18-08-2022 task 1] use @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test';
+
+  //@ViewChild('myText', {static: false}) myTest!: ElementRef;
+  @ViewChild('myText') myTest!: ElementRef;
+
+  send(data: any) {
+    console.log('data: ', data.value);
+    console.log('MY TEST IN TS: ', this.myTest);
+    this.myTest.nativeElement.value = 'cambio desde el ts'
+  }
 }
