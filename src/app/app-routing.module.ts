6a322e3602ee38a591cad4f6a268c8cf8af849ce
rsoft-latam <rikiricardo16@hotmail.com>
2022-08-24 17:15:04 -0400

    [24-08-2022 task 1] basic navigation with components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { View1Component } from './dashboard/view1/view1.component';
+import { View2Component } from './dashboard/view2/view2.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'view1',
+        component: View1Component
+      },
+      {
+        path: 'view2',
+        component: View2Component
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { View1Component } from './dashboard/view1/view1.component';
+import { View2Component } from './dashboard/view2/view2.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    View1Component,
+    View2Component
   ],
   imports: [
     BrowserModule,
