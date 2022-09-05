28df4e4673c48a8fb8063f7927db6979e062d492
Ricardo Pari <rikiricardo16@hotmail.com>
2022-09-05 17:48:04 -0400

    [05-09-2022 task 2] basic example reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test';
-
+  public colorControl = new FormControl('');
+  constructor() {
+  }
+  ngOnInit() {
+    this.colorControl.valueChanges.subscribe(value => {
+      console.log('VALUE CHANGES: ', value)
+    })
+  }
   onSubmit(form:any){
     console.log('FORM: ', form)
   }
-
+  printColorControl(){
+    console.log(this.colorControl)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import {FormsModule} from "@angular/forms";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
