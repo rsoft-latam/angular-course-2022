396000084d3e3984a13bd0614307690cff5ed2bb
Ricardo Pari <rikiricardo16@hotmail.com>
2022-09-07 18:14:52 -0400

    [07-09-2022 task 2] validations on reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+
+  formR!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.formR = this.formBuilder.group({
+        name: ['', [Validators.required, Validators.minLength(4)]],
+        lastName: '',
+        country: ''
+      }
+    )
+
+  }
+
+
 }
