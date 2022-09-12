a9b42f0f9e2cc8fae2741147da9382dad97d8b99
Ricardo Pari <rikiricardo16@hotmail.com>
2022-09-12 17:39:58 -0400

    [12-09-2022 task 1] guard with recirect

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,18 +1,25 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from "../../pages/services/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
+  constructor(private authService: AuthService,
+              private router: Router) {
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
+    const token = this.authService.getToken();
+    if (token) {
+      return true;
+    }
+    this.router.navigate(['login']);
     return false;
-
   }
-
 }
