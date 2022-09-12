f47ab47af63e12a96529746b312bd1b5eda4055d
Ricardo Pari <rikiricardo16@hotmail.com>
2022-09-12 18:33:29 -0400

    [12-09-2022 task 3] logout in header}

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CarService} from "../services/car.service";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   templateUrl: './home.component.html',
@@ -8,13 +9,22 @@ import {CarService} from "../services/car.service";
 export class HomeComponent implements OnInit {
   title = 'test';
 
-  constructor(private carService: CarService) {
+  constructor(private carService: CarService,
+              private authService: AuthService) {
   }
 
   ngOnInit() {
     this.carService.getAllCars().subscribe(res => {
       console.log('RESPONSE CARS: ', res)
     })
+  }
+
+  /**
+   * Logout to go to Login view
+   * @public
+   */
+  public onLogout(): void {
+    this.authService.logout();
   }
 
 
