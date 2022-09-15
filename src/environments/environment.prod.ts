c2d7b9452c3155dffff13fe826ad1263b0a5a7b8
Ricardo Pari <rikiricardo16@hotmail.com>
2022-09-15 16:17:18 -0400

    fake data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  app: {
+    apiBaseUrl: 'https://league-of-legends-upb-default-rtdb.firebaseio.com'
+  },
+  auth: {
+    apiBaseUrl: 'https://identitytoolkit.googleapis.com',
+    apiKey: 'AIzaSyAhmKgPPOLgdzW_PyvNx1VNMzsabLiCYKI'
+  }
 };
